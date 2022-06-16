8be3febdb4b20f7b8c2fee29764e5b86e60d3f25
BasicInputOutputSystem <106867313+BasicInputOutputSystem@users.noreply.github.com>
2022-06-16 08:10:01 -0500

    Compression.ps1

--- CET/comp.ps1
+++ CET/comp.ps1
@@ -1,4 +1,4 @@
-$PATH = "C:/Program Files/CET"
+$PATH = "C:/Program Files/CET-Compression"
 Invoke-WebRequest https://www.github.com/BasicInputOutputSystem/CET-compression-tool/CET.zip -Outfile CET.zip
 Expand-Archive CET.zip -Destination $PATH
 $PATH = "C:/Program Files/CET/CET.exe"
